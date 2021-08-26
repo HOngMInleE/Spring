ddc6bb04164cc6e34ee28f0533931db6eee83506
ori34 <ori34@DESKTOP-TKEDJGN>
2021-08-26 10:35:18 +0900

    jdbc Library추가, 설정

---
+++ 0824_BoardWeb2/src/main/java/com/springbook/biz/impl/BoardDAOSpring.java
@@ -0,0 +1,11 @@
+package com.springbook.biz.impl;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BoardDAOSpring extends JdbcDaoSupport {
+
+	
+	
+}
